08127c0b71ec9f159c0dede662048565683b9b1d
Chathuka-Pehesara <ChathukaPehesara02@gmail.com>
2026-06-19 12:32:40 +0530

    Created Payment Model

---
+++ server/models/Payment.js
@@ -0,0 +1,16 @@
+const monngoose = required('mongoose')
+
+const paymentSchema = new mongoose.Schema({
+    order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
+    customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    payhereOrderId: { type: String, required: true, unique: true },
+    amount: { type: Number, required: true },
+    currency: { type: String, default: 'LKR' },
+    statusCode: { type: String, default: '' },
+    statusMessage: { type: String, default: '' },
+    method: { type: String, default: '' },
+    cardNo: { type: String, default: '' },
+    paidAt: { type: Date },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Payment', paymentSchema);

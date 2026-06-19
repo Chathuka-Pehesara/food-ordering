7ea4029e0de6c6d51a010a1b898d211c1391d6dd
Chathuka-Pehesara <ChathukaPehesara02@gmail.com>
2026-06-19 11:44:03 +0530

    update index js with basic configiration

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.Mongo_URI)
+    .then(() => console.log('MongoDB connected'));
+
+app.listen(5000, () => console.log('Sever running on port 5000'));

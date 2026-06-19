64e21a1b46f19b0850d1288a7d21d1e57dec08f2
Chathuka-Pehesara <ChathukaPehesara02@gmail.com>
2026-06-19 11:49:41 +0530

    Created User Model

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongosoe')
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: { type: String, enum: ['customer', 'admin'], default: 'customer' }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('User', userSchema);

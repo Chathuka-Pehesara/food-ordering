c45aea4095c3b57f5b4d89dcd8118dbb9719e030
Chathuka-Pehesara <ChathukaPehesara02@gmail.com>
2026-06-19 12:43:11 +0530

    Updated FoodItem model

--- server/models/FoodItem.js
+++ server/models/FoodItem.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const foodItemSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    price: { type: Number },
-    category: { type: String, enum: ['Pizza', 'Burger', 'Cake'] },
-    image: { type: String },
+    name: { type: String, required: true, trim: true },
+    description: { type: String, default: '' },
+    price: { type: Number, required: true, min: 0 },
+    category: { type: String, enum: ['Pizza', 'Burger', 'Cake', 'Other'], deafault: 'Other' },
+    image: { type: String, default: '' },
     available: { type: Boolean, default: true }
-}, { timestamps: true })
+}, { timestamps: true });
 
 module.exports = mongoose.model('FoodItem', foodItemSchema);
